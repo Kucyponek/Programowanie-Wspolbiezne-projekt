f0dfda67ae96d3955ef20d81bc0df57b8ec996f7
Kucyponek <35694114+Kucyponek@users.noreply.github.com>
2020-04-27 12:46:02 +0200

    Add files via upload

---
+++ Server/ChatMessageHandler.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    public class ChatMessageHandler : IObserver<Object>
+    {
+        readonly ChatRoomManager _chatRoomManager;
+        readonly ChatSession _session;
+
+        public ChatMessageHandler(ChatRoomManager chatRoomManager, ChatSession session)
+        {
+            _chatRoomManager = chatRoomManager;
+            _session = session;
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("ChatMessage: Zakonczono");
+            _chatRoomManager.RemoveFromRoom(_session);
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("ChatMessage: " + error.Message);
+            _chatRoomManager.RemoveFromRoom(_session);
+        }
+
+        public void OnNext(Object omsgIn)
+        {
+            dynamic msgIn = omsgIn;
+            String roomName = msgIn.room;
+            ChatRoom chatRoom;
+            if (_chatRoomManager.TryGetValue(roomName, out chatRoom))
+            {
+                if (chatRoom.Contains(_session))
+                {
+                    msgIn.nick = _session.Nick;
+                    msgIn.timestamp = DateTime.Now.ToString("hh:mm:ss");
+                    foreach (var client in _chatRoomManager[roomName])
+                        client.Out.OnNext(msgIn);
+                }
+                else
+                    throw new ArgumentException("Uzytkownika nie ma na czacie");
+            }
+        }
+    }
+}
